c8dc7153781f316703887215bcfc03cc1ab532b3
ediele.ediele <ediele.ediele@hotmail.com>
2020-05-25 23:45:29 -0300

    Initial project files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { MenuComponent } from './navigation/menu/menu.component';
+import { HomeFirstPartComponent } from './home/home-first-part/home-first-part.component';
+import { HomeSecondPartComponent } from './home/home-second-part/home-second-part.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeFirstPartComponent,
+    HomeSecondPartComponent
   ],
   imports: [
     BrowserModule
